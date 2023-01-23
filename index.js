75cd45055ae793b6bd16978ae5a87d8cb3f30a88
Cipriano Mereu <ciprianomereu@gmail.com>
2023-01-23 19:40:40 +0100

    add:exercise 22

---
+++ index.js
@@ -0,0 +1,19 @@
+// # Destructuring assignment - 22
+
+// Use the destructuring to assign the values just with one code line.
+
+const person = {
+  id: 1,
+  firstName: "Mario",
+  lastName: "Rossi",
+  age: 25,
+};
+
+// const id = person.id;
+// const firstName = person.firstName;
+// const lastName = person.lastName;
+// const age = person.age;
+
+const { id, firstName, lastName, age } = person;
+
+console.log(id, firstName, lastName, age);

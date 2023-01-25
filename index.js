6301105c13c23cecb898e9fab8d2326a789f40fb
Cipriano Mereu <ciprianomereu@gmail.com>
2023-01-26 00:01:16 +0100

    fix

--- index.js
+++ index.js
@@ -3,8 +3,8 @@
 // Use the destructuring, instead of a temporary variable, to swap the values of the variables.
 //
 //
-const a = [10, 20];
-let [num1, num2] = a;
+
+let [num1, num2] = [10, 20];
 
 // let num1 = 10;
 // let num2 = 20;
@@ -16,6 +16,6 @@ console.log("Before swap: ", num1, num2); // Before swap: 10 20
 // num2 = num1;
 // num1 = temp;
 
-[num1, num2] = [a[1], a[0]];
+[num1, num2] = [num2, num1];
 
 console.log("After swap: ", num1, num2); // After swap: 20 10

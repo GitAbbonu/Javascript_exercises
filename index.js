40a5e103a965dca8da1e921eeacb1f99d56dde86
Cipriano Mereu <ciprianomereu@gmail.com>
2023-01-25 12:41:02 +0100

    add:exercise 25

---
+++ index.js
@@ -0,0 +1,16 @@
+// # Rest parameters and spread syntax
+
+// Do you know how to "merge" the value of the `newNumber ` variable into `numberStore` without using the array method push?
+
+let numberStore = [0, 1, 2];
+// let newNumber = 3;
+
+function add(numStore, num) {
+  return (numStore = [...numStore, num]);
+}
+
+numberStore = add(numberStore, 3);
+
+// numberStore.push(newNumber);
+console.log(numberStore);
+// console.log(newStore);

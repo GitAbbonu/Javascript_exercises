74f27cd682b0fb585742fd9f9ca624377221d913
Cipriano Mereu <ciprianomereu@gmail.com>
2023-01-25 12:48:13 +0100

    add: exercise 26

---
+++ index.js
@@ -0,0 +1,14 @@
+// # Rest parameters and spread syntax - Exercise 65
+
+// The `sum` function has a high numbers of parameters. How can we improve the code in order to make it accept an unknown numbers to sum?
+
+// function sum(num1, num2, num3, num4, num5) {
+//   return num1 + num2 + num3 + num4 + num5;
+// }
+function sum(...nums) {
+  return nums.reduce((cont, v) => cont + v);
+
+  //   return num1 + num2 + num3 + num4 + num5;
+}
+
+console.log(sum(1, 2, 3, 4, 5));

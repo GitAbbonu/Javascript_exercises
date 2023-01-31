9e38cf4f4337ef52f9ccd9174c02070ba1c9cb3f
Cipriano Mereu <ciprianomereu@gmail.com>
2023-01-31 17:08:51 +0100

    add:exercise 33

---
+++ index.js
@@ -0,0 +1,16 @@
+// Perform a code refactoring by using the Template Literals.
+
+class Person {
+  constructor(firstName, lastName) {
+    this.firstName = firstName;
+    this.lastName = lastName;
+  }
+
+  toString() {
+    return `${this.firstName} ${this.lastName}`;
+  }
+}
+
+const developer = new Person("Mario", "Rossi");
+console.log(developer.toString());
+// console.log(developer.firstName + " " + developer.lastName);

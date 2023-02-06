e731db17f01d98e13ac92af2724b0c3096746d90
Cipriano Mereu <ciprianomereu@gmail.com>
2023-02-06 12:56:55 +0100

    add:exercise 35

---
+++ index.js
@@ -0,0 +1,23 @@
+// # Exercise Async Callback
+
+// Write a function called `repeatHello`, which will take as parameters a callback.
+// The callback function will simply print "Hello".
+// The `repeatHello` function will have to execute the callback function with an interval of 1 second.
+
+// Tips:
+
+// - The `setInterval` method will be useful
+
+function sayHello() {
+  return "Hello";
+}
+
+function repeatHello(callback) {
+  const int = setInterval(() => console.log(callback()), 1000);
+
+  setTimeout(() => {
+    clearInterval(int);
+  }, 10000);
+}
+
+repeatHello(sayHello);

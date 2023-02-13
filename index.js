04b311cc0d02b9476c4944fd07036704442df9dd
Cipriano Mereu <ciprianomereu@gmail.com>
2023-02-13 13:23:08 +0100

    add:exercise 41

--- index.js
+++ index.js
@@ -1,10 +1,4 @@
-// # Exercise Local Storage
-
-// In the `exercise.js` file, we find a `user` object. Write a function that saves the user object in `localStorage` with its 'user' key.
-
-// Tips:
-
-// - Remember to use the available `JSON methods` (parse, stringify)
+// Starting from the previous exercise, write a method that recovers the data saved in localStorage and prints them in the console.
 
 const user = {
   id: 1,
@@ -26,4 +20,29 @@ function getItem(item) {
   return obj;
 }
 
-console.log(getItem("user"));
+function printItems() {
+  console.log(localStorage);
+}
+
+function printLocalStorage() {
+  for (var i = 0; i < localStorage.length; i++) {
+    var key = localStorage.key(i);
+    var value = localStorage.getItem(key);
+    console.log(key + ": " + value);
+  }
+  //   for (var key in localStorage) {
+  //     if (
+  //       key !== "length" &&
+  //       key !== "clear" &&
+  //       key !== "getItem" &&
+  //       key !== "key" &&
+  //       key !== "removeItem" &&
+  //       key !== "setItem"
+  //     ) {
+  //       console.log(key + " = " + localStorage[key]);
+  //     }
+  //   }
+}
+
+// printItems();
+printLocalStorage();
